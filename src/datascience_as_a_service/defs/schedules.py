938a910ba479ae487d0618a99faeda092b4d955d
Leon David Zipp <leondavidzipp@gmx.de>
2026-04-10 08:54:21 +0200

    feat: initial trigger schedule for raw (file based) resources

--- src/datascience_as_a_service/defs/schedules.py
+++ src/datascience_as_a_service/defs/schedules.py
@@ -6,3 +6,13 @@ def daily_ingest_schedule(
     context: dg.ScheduleEvaluationContext,
 ) -> dg.RunRequest | dg.SkipReason:
     return dg.RunRequest()
+
+
+INTERVAL_MINUTES = 60 * 5
+
+
+@dg.sensor(target="*", minimum_interval_seconds=INTERVAL_MINUTES)  # type: ignore[reportUnknownVariableType]
+def raw_sensor(
+    context: dg.SensorEvaluationContext,
+) -> dg.RunRequest | dg.SkipReason:
+    return dg.RunRequest()
