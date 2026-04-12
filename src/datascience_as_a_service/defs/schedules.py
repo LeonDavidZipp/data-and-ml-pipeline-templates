a822d11613ac3e99472737167abbd4f4e315f59a
Leon David Zipp <leondavidzipp@gmx.de>
2026-04-12 14:57:59 +0200

    fix: raw_sensor now named bronze_sensor

--- src/datascience_as_a_service/defs/schedules.py
+++ src/datascience_as_a_service/defs/schedules.py
@@ -12,7 +12,7 @@ INTERVAL_MINUTES = 60 * 5
 
 
 @dg.sensor(target="*", minimum_interval_seconds=INTERVAL_MINUTES)  # type: ignore[reportUnknownVariableType]
-def raw_sensor(
+def bronze_sensor(
     context: dg.SensorEvaluationContext,
 ) -> dg.RunRequest | dg.SkipReason:
     return dg.RunRequest()
